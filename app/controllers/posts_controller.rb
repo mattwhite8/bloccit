c5dd0e8119f8c7cbb03df64d7b36245cc7b8a076
mattwhite8 <mattwhite1337@gmail.com>
2015-02-22 17:21:55 +0000

    Complete Authorization Assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all 
+    p current_user
+    @posts = PostPolicy::Scope.new(current_user, Post).resolve
     authorize @posts
   end
 
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,16 @@
 class PostPolicy < ApplicationPolicy
   
+  class Scope < Scope
+            
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where("user_id = ?", user.id)
+      end
+    end
+  end
+  
   def index?
     true
   end
