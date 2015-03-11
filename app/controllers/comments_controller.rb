09dfa7dd1d15d9868503329358b94382e13f9269
mattwhite8 <mattwhite1337@gmail.com>
2015-03-11 20:59:25 +0000

    Added comment functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+  
+  def create 
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.create(comment_params_with_post)
+    @new_comment = Comment.new
+    if @comment.save
+      flash[:notice] = "Comment was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+      redirect_to [@topic, @post]
+    end
+    
+  end
+  
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+  
+  def comment_params_with_post
+    comment_params.merge(post: @post)
+  end
+  
+end
