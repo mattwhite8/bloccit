0831f873ebe55975551a7c7698b7a5d04f592eec
mattwhite8 <mattwhite1337@gmail.com>
2015-03-18 16:47:40 +0000

    Expanded testing

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,24 +1,33 @@
 require 'rails_helper'
 
 describe Vote do
+  
+  include TestFactories
+  
   describe "validations" do
-    
-    before do 
-      @vote = Vote.create(value: -1)
-      @vote_two = Vote.create(value: 1)
-    end
-    
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(@vote.value).to eq(-1) 
+        up_vote = Vote.new(value: 1)
+        expect(up_vote.valid?).to eq(true)
+
+        down_vote = Vote.new(value: -1)
+        expect(down_vote.valid?).to eq(true)
+
+        invalid_vote = Vote.new(value: 2)
+        expect(invalid_vote.valid?).to eq(false)
       end
     end
-    
-     describe "value validation" do
-      it "only allows -1 or 1 as values" do
-        expect(@vote_two.value).to eq(1) 
-      end
-    end
-    
   end
+  
+  describe 'after_save' do
+    it "call 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
+  
+        
+  
 end
