29f6965dedc4735ba04956654c2711356d5fc8f0
mattwhite8 <mattwhite1337@gmail.com>
2015-03-20 17:19:10 +0000

    Complete private topics assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,16 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy   
   
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :visible_to, -> (user) { user ? all : publicly }
   
   validates :name, length: {minimum: 5 }, presence: true 
+  
+  def self.publicly
+    where(public: true)
+  end
+  
+  def self.privately
+    where(public: false)
+  end
+    
 end
