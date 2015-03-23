e2befc308d7ebd0babb2d9d91ce22e0473e63631
mattwhite8 <mattwhite1337@gmail.com>
2015-03-23 17:40:56 +0000

    Complete another interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,13 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe    
   end
+  
+  def up_vote_link_classes(post)
+    p "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+  
+  def down_vote_link_classes(post)
+    p  "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
+  
 end
